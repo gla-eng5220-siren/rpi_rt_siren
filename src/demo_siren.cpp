6a767b8d459db6737be49dfd70298d0cbb132f33
Keyu_Shi_2995922 <keleguan553@gmail.com>
2026-04-01 11:16:52 +0100

    Use W1 temperature sensor in demo_siren
    Replace the mock temperature sensor with the real W1 temperature sensor in demo_siren and adjust the runtime for testing.

--- src/demo_siren.cpp
+++ src/demo_siren.cpp
@@ -1,32 +1,37 @@
 #include "alarm.hpp"
 #include "sensor.hpp"
-#include <thread>
+
+#include <chrono>
 #include <iostream>
+#include <thread>
 
 int main(void) {
-  auto alarm = rpi_rt::create_stdout_alarm();
-  auto sensor = rpi_rt::create_mock_temperature_sensor();
+    auto alarm = rpi_rt::create_stdout_alarm();
+    auto sensor = rpi_rt::create_w1_temperature_sensor();
 
-  sensor->set_celsius_reciever([&alarm](float degrees) {
-    std::cout << "We got a celsius degree: " << degrees << std::endl;
-    if (degrees > 200.0f) {
-      alarm->report_fire();
-    }
-  });
+    sensor->set_celsius_reciever([&alarm](float degrees) {
+        std::cout << "We got a celsius degree: " << degrees << std::endl;
 
-  std::thread alarm_thread([self = alarm](){
-    self->run();
-  });
-  std::thread sensor_thread([self = sensor](){
-    self->run();
-  });
+        if (degrees > 60.0f) {
+            alarm->report_fire();
+        }
+    });
 
-  std::this_thread::sleep_for(std::chrono::seconds{5});
+    std::thread alarm_thread([self = alarm]() {
+        self->run();
+    });
 
-  alarm->close();
-  sensor->close();
-  alarm_thread.join();
-  sensor_thread.join();
-  return 0;
+    std::thread sensor_thread([self = sensor]() {
+        self->run();
+    });
+
+    std::this_thread::sleep_for(std::chrono::seconds{30});
+
+    alarm->close();
+    sensor->close();
+
+    alarm_thread.join();
+    sensor_thread.join();
+
+    return 0;
 }
-
