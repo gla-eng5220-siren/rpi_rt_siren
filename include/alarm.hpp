7b64daabd30ac13ab5aed51b2f7e00ffd37c4ecd
youcai <omegacoleman@gmail.com>
2026-04-08 10:28:07 +0100

    add email alarm with brevo API

--- include/alarm.hpp
+++ include/alarm.hpp
@@ -2,10 +2,23 @@
 
 #include <memory>
 #include <functional>
+#include <string>
 
 #include "detection_result.hpp"
 
 namespace rpi_rt {
+
+  struct brevo_config_t {
+    std::string api_host = "api.brevo.com";
+    std::string api_path = "/v3/smtp/email";
+
+    std::string api_key;
+    std::string sender_name = "FlameIris Webhook";
+    std::string sender_email;
+    std::string to_email;
+    std::string to_name = "FlameIris User";
+  };
+
   class alarm_t {
     public:
       virtual ~alarm_t() {}
@@ -15,5 +28,6 @@ namespace rpi_rt {
   };
 
   std::shared_ptr<alarm_t> create_stdout_alarm();
+  std::shared_ptr<alarm_t> create_brevo_email_alarm(brevo_config_t cfg);
 }
 
--- include/detection_result.hpp
+++ include/detection_result.hpp
@@ -1,11 +1,17 @@
 #pragma once
 
+#include <vector>
+#include <string>
+
 namespace rpi_rt {
   class detection_result_t {
     public:
       virtual ~detection_result_t() {}
       virtual bool has_fire() = 0;
       virtual std::string explain() = 0;
+      virtual std::vector<uint8_t> jpg_attachment() {
+        return {};
+      };
   };
 }
 
